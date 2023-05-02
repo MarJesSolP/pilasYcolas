07c88eb5aa3ab6ceb9c294f7b562205c08999919
MarJesSolP <usr3597@salesianos-lacuesta.net>
2023-05-02 13:15:16 +0100

    fix:modificado el array para hacerlo privado y estático

--- src/es/hiperdino/utilidades/generadorRandom.java
+++ src/es/hiperdino/utilidades/generadorRandom.java
@@ -3,14 +3,14 @@ package es.hiperdino.utilidades;
 import java.util.Random;
 
 public class generadorRandom {
+    private static final String [] PRODUCTOS = { "Atún Bonito", "Vieja", "Aceite Masiasol","YoPro","ColaCao","Tinto de Verano Don Simon","Bezoya","Nestea Mango Piña","Coca Cola Zero","Lactovit","Munchitos","Queso de Valsequillo"
+    };
     public static String productosAleatorios(){
-        final String [] PRODUCTOS = { "Atún Bonito", "Vieja", "Aceite Masiasol","YoPro","ColaCao","Tinto de Verano Don Simon","Bezoya","Nestea Mango Piña","Coca Cola Zero","Lactovit","Munchitos","Queso de Valsequillo"
-        };
+
         Random random = new Random();
         int indexAleatorio = random.nextInt(PRODUCTOS.length);
         return PRODUCTOS[indexAleatorio];
     }
-
 
 
 }
