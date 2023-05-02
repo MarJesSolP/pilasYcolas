bb9b4e34d8982a4c1cb52c32aaa7e87dc9f9678f
MarJesSolP <usr3597@salesianos-lacuesta.net>
2023-05-02 13:28:15 +0100

    feat(generador-Ramdom): Añadido el método para  generar un nombre aleatorio

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
+import static es.hiperdino.utilidades.generadorRandom.nombreAleatorio;
 import static es.hiperdino.utilidades.generadorRandom.productosAleatorios;
 
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(nombreAleatorio());
         System.out.println(productosAleatorios());
     }
 
--- src/es/hiperdino/utilidades/generadorRandom.java
+++ src/es/hiperdino/utilidades/generadorRandom.java
@@ -5,12 +5,17 @@ import java.util.Random;
 public class generadorRandom {
     private static final String [] PRODUCTOS = { "Atún Bonito", "Vieja", "Aceite Masiasol","YoPro","ColaCao","Tinto de Verano Don Simon","Bezoya","Nestea Mango Piña","Coca Cola Zero","Lactovit","Munchitos","Queso de Valsequillo"
     };
+    private static final String [] NOMBRES = {"Naira", "Magec", "Idaira", "Yeray", "Yurena", "Txenery", "Idaira", "Airam", "Ayoze", "Nauzet", "Fayna", "Gara", "Ancor", "Bentor", "Iballa", "Ico", "Isora", "Aday", "Haridian", "Arai"};
     public static String productosAleatorios(){
 
         Random random = new Random();
         int indexAleatorio = random.nextInt(PRODUCTOS.length);
         return PRODUCTOS[indexAleatorio];
     }
-
+    public static String nombreAleatorio(){
+        Random random = new Random();
+        int indexAleatorio = random.nextInt(NOMBRES.length);
+        return NOMBRES[indexAleatorio];
+    }
 
 }
