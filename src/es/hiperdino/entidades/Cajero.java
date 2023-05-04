8fc3551d02a4ed11f7a38e6497d2aa3afb3b7c87
MarJesSolP <usr3597@salesianos-lacuesta.net>
2023-05-04 10:18:50 +0100

    feat: añadida la clase cajero con método para abrir y cerrar caja, además dagregar los clientes a la fila y mostrar los clientes

--- src/es/hiperdino/entidades/Cajero.java
+++ src/es/hiperdino/entidades/Cajero.java
@@ -1,4 +1,39 @@
 package es.hiperdino.entidades;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Cajero {
+    private int numeroCaja, identificadorDeCaja;
+    private boolean abrirCaja = false;
+
+    Queue <String> nombreClientesFilaDeCaja = new LinkedList<>();
+    public boolean getAbrirCaja() {
+        return abrirCaja;
+    }
+    public void abrirCaja(){
+        if(abrirCaja == true){
+            System.out.println("Shiquilla ya está abierta");
+        }else {
+            abrirCaja = true;
+            System.out.println("Caja abierta");
+        }
+    }
+    public void cerrarCaja(){
+        if(!abrirCaja){
+            System.out.println("La caja ya está cerrada MOSTRO");
+        }else {
+            System.out.println("Cerrando la cajita...");
+            abrirCaja = false;
+        }
+    }
+    public void agregarNombresClientes(String nombre){
+        nombreClientesFilaDeCaja.add(nombre);
+    }
+    public void verClienteFila(){
+        System.out.println("Los clientes en fila " + nombreClientesFilaDeCaja);
+    }
+
+
+
 }
